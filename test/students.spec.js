a4e4b0f59ffd639b3bb7718b9a6186c1b11607b7
Ndekocode <arickbulakali@gmail.com>
2023-01-18 16:04:17 +0200

    test: first test with add student object

---
+++ test/students.spec.js
@@ -0,0 +1,12 @@
+import { should } from "chai";
+import Student from "../src/Student";
+
+should();
+
+describe("Student", () => {
+  describe("#Student", () => {
+    it("Should allow me to allow me to create a student", () => {
+      Student("Arick", "Bulakali");
+    });
+  });
+});
