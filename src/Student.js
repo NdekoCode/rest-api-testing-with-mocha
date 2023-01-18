3af88f07b5061809d6698856728f8059fcb0d4eb
Ndekocode <arickbulakali@gmail.com>
2023-01-18 19:59:53 +0200

    test: add student presentation and verify is is define

--- src/Student.js
+++ src/Student.js
@@ -1,6 +1,13 @@
 export default class Student {
-  constructor(firstName, lastName) {
+  constructor(firstName, lastName, age = 18) {
     this.firstName = firstName;
     this.lastName = lastName;
+    this.age = age;
+  }
+  presentation() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+  getAge() {
+    return this.age;
   }
 }
--- test/students.spec.js
+++ test/students.spec.js
@@ -1,12 +1,21 @@
-import { should } from "chai";
+import { expect, should } from "chai";
 import Student from "../src/Student.js";
 
 should();
 
 describe("Student", () => {
-  describe("#Student", () => {
+  describe("#Student initiation", () => {
     it("Should allow me to allow me to create a student", () => {
-      new Student("Arick", "Bulakali");
+      const student = new Student("Arick", "Bulakali");
+      student.should.not.be.equal(undefined);
+    });
+    it("Should present student", () => {
+      const alxStd = new Student("Alex", "Amani");
+      alxStd.presentation().should.be.equal("Alex Amani");
+    });
+    it("Should gave us the age of student", () => {
+      const newStd = new Student("ndekocode", "Bum", 20);
+      expect(newStd.getAge()).to.be.equal(20);
     });
   });
 });
