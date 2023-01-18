be23b937ca43165471a668d04bb0d9da593ae99a
Ndekocode <arickbulakali@gmail.com>
2023-01-18 16:10:11 +0200

    fixed: add new student test

---
+++ src/Student.js
@@ -0,0 +1,6 @@
+export default class Student {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+}
--- test/students.spec.js
+++ test/students.spec.js
@@ -1,12 +1,12 @@
 import { should } from "chai";
-import Student from "../src/Student";
+import Student from "../src/Student.js";
 
 should();
 
 describe("Student", () => {
   describe("#Student", () => {
     it("Should allow me to allow me to create a student", () => {
-      Student("Arick", "Bulakali");
+      new Student("Arick", "Bulakali");
     });
   });
 });
