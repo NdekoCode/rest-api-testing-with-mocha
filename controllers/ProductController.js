98042ac9a3859590baad8c04b5259e1375fa3da7
Ndekocode <arickbulakali@gmail.com>
2023-01-19 21:17:21 +0200

    setup: add ProductController class structure

---
+++ controllers/ProductController.js
@@ -0,0 +1,10 @@
+export default class ProductController {
+  async getProducts(req, res) {}
+  async getSingleProduct(req, res) {}
+
+  async postProduct(req, res) {}
+
+  async updateProduct(req, res) {}
+
+  async deleteProduct(req, res) {}
+}
