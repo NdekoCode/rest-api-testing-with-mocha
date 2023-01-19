0cf5104250b85956871c7282aacfb100dc923fb0
Ndekocode <arickbulakali@gmail.com>
2023-01-19 21:41:04 +0200

    docs: add endpoints documentation

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -3,10 +3,26 @@ import ProductController from "../controllers/ProductController.js";
 const productRouter = Router();
 
 const productCTRL = new ProductController();
-productRouter.get("/", productCTRL.getProducts);
+// -- CRUD Operations
+
+// /api/v1/products
+// Create product - POST
 productRouter.post("/", productCTRL.postProduct);
+
+// /api/v1/products
+// Read all products -- GET
+productRouter.get("/", productCTRL.getProducts);
+
+// /api/v1/products/:id
+// Read specific product -- GET
 productRouter.get("/:id", productCTRL.getSingleProduct);
+
+// /api/v1/products/:id
+// UPDATE specific product -- PUT
 productRouter.put("/:id", productCTRL.updateProduct);
+
+// /api/v1/products/:id
+// DELETE specific product -- PUT
 productRouter.delete("/:id", productCTRL.deleteProduct);
 
 export default productRouter;
