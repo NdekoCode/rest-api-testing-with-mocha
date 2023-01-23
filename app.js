941a9ed0823c4c3480a6588f6e15d0719aff1162
Ndekocode <arickbulakali@gmail.com>
2023-01-23 20:12:31 +0200

    refactor: swagger.yaml for output auth docs

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import authRouter from "./routes/auth.routes.js";
 import productRouter from "./routes/product.routes.js";
 import { BASE_API } from "./utils/constants.js";
 dotenv.config();
+
 const app = express();
 app.use(express.json());
 app.use(
@@ -14,16 +15,20 @@ app.use(
     extended: true,
   })
 );
+// On configure la documentation de l'API
 const swaggerDefinition = yamljs.load("./swagger.yaml");
+
 app.use(
   BASE_API + "/docs",
   swaggerUI.serve,
   swaggerUI.setup(swaggerDefinition)
 );
 const { home } = new MainController();
+
 // ROUTES
 // POST, GET, DELETE --> CRUD
 app.use(BASE_API + "/products", productRouter);
+
 // api/v1/user
 app.use(BASE_API + "/user", authRouter);
 app.use(BASE_API, home);
