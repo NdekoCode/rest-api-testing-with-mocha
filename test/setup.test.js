854fa92c6d44739cb84040b0cd503a8015e72457
Ndekocode <arickbulakali@gmail.com>
2023-01-24 18:40:41 +0200

    test: main setup begin test project

--- test/setup.test.js
+++ test/setup.test.js
@@ -3,13 +3,16 @@ import chaiHttp from "chai-http";
 import { before } from "mocha";
 import server from "../app.js";
 import ProductModel from "../models/ProductModel.js";
+import UserModel from "../models/UserModel.js";
 chai.use(chaiHttp);
 before((done) => {
   ProductModel.deleteMany({}, (err) => {});
+  UserModel.deleteMany({}, (err) => {});
   done();
 });
 after((done) => {
   ProductModel.deleteMany({}, (err) => {});
+  UserModel.deleteMany({}, (err) => {});
   done();
 });
 
