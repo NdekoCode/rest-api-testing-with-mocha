5cebd7595ff8eeb830a1c9469db71ea93271124a
Ndekocode <arickbulakali@gmail.com>
2023-01-24 10:08:33 +0200

    test: verify if the schema product is Empty

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # This dans les controllers
 
 Il n'y a aucun moyen de prendre en charge les "classes ES2015" lors du routage, ceci est au niveau de JavaScript. Lorsque vous appelez une fonction, cela est défini à la valeur laissée par la fonction au moment de l'appel. Dans ce cas, ce sera indéfini. Ce que vous devez faire, c'est éviter d'utiliser ce paramètre lorsque vous n'avez aucun contrôle sur l'endroit où la fonction est exécutée, ou lier vous-même le contexte de la méthode.
+
+## Quelques conseils et astuces sur les tests
+
+Avant de tester, c'est une bonne pratique d'effacer d'abord la base de donnée donc il faut toujours avoir une base de donnée pour les test et une autre pour le developpement et enfin une pour la production et pour cela j'ai utiliser les fonction `before`,`after`, `beforeEach` et `afterEach` de `mocha`
--- test/product.test.js
+++ test/product.test.js
@@ -1,9 +1,18 @@
 import chai, { expect } from "chai";
 import chaiHttp from "chai-http";
 import server from "../app.js";
+import ProductModel from "../models/ProductModel.js";
 import { BASE_API } from "../utils/constants.js";
 chai.use(chaiHttp);
-describe("/FIRST Test collection", () => {
+before((done) => {
+  ProductModel.deleteMany({}, (err) => {});
+  done();
+});
+after((done) => {
+  ProductModel.deleteMany({}, (err) => {});
+  done();
+});
+describe("/FIRST TEST collection", () => {
   it("It should allow me to go to the main page", (done) => {
     chai
       .request(server)
@@ -12,6 +21,17 @@ describe("/FIRST Test collection", () => {
         expect(res.status).to.be.equal(200);
         expect(res.body).to.be.a("object");
         expect(res.body.message).to.be.equal("Welcome to the MEN RESTFUL APP");
+        done();
+      });
+  });
+  // Help us to check how many product we have in the database
+  it("Should verify that we have 0 products in the DB", (done) => {
+    chai
+      .request(server)
+      .get(BASE_API + "/products")
+      .end((err, res) => {
+        expect(res.status).to.be.equal(200);
+        expect(res.body).to.be.a("array").with.lengthOf(0);
         done();
       });
   });
